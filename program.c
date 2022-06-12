83f43a3f4a330c18d605d1f3e1748da523c9c765
Oscar Guertler <oscarguertler@gmail.com>
2022-06-12 12:12:45 +0200

    def max text len

--- program.c
+++ program.c
@@ -1,9 +1,12 @@
 #include "modal.h"
+#include <string.h>
+#include <stdio.h>
 
 int main()
 {
-    char *msg = "Test 123asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasfas";
     ModalGeometry modalGeometry = { .border = 2, .height = 50, .width = 400, .insideX = 10, .insideY = 27 };
+    char msg[MAX_MESSAGE_LEN(2, 400, 10, 13)] = "Test 123asdfasdfasdfasdfasaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+
     ModalColors modalColors = { .background = "#0f111b", .foreground = "#ecf0c1" };
     Modal *modal = create_modal(msg, &modalGeometry, &modalColors);
 
